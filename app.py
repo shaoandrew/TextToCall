4b12473e542e6cde795759634bb609c12e76d61d
Allan Zhou <allan.zhou@berkeley.edu>
2015-03-01 17:29:11 -0800

    Connected index.html

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
 import os, requests, json
-from flask import Flask, Response, request, url_for
+from flask import Flask, Response, request, url_for, send_from_directory
 import plivoxml, plivo
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
-@app.route('/call')
+@app.route('/')
+def index():
+    return send_from_directory('static', 'index.html')
+
+@app.route('/call', methods=['POST'])
 def call():
+    num = request.form['number']
+    msg = request.form['message']
     auth_id = ""
     auth_token = ""
     p = plivo.RestAPI(auth_id, auth_token)
     params = {
         'from': '14157232470',
-        'to':'19169903451',
+        'to': num,
         'answer_url':'http://pacific-stream-4609.herokuapp.com/response/speak/',
         'answer_method': 'GET'
     }
     r = p.make_call(params)
     return str(r)
 
-@app.route('/response/speak/', methods=['GET'])
+@app.route('/response/speak/', methods=['POST'])
 def speak():
     # Enter the message you want to play
     text = "You are a total scrub."
@@ -29,15 +35,15 @@ def speak():
 
     return Response(str(response), mimetype='text/xml')
 
-def insultreq():
-    requestUrl = 'http://quandyfactory.com/insult/json'
-    page = urllib.urlopen(requestUrl)
-	contents = page.read()
-	insult = json.loads(contents)
-	return insult['insult']
+#def insultreq():
+#    requestUrl = 'http://quandyfactory.com/insult/json'
+#    page = urllib.urlopen(requestUrl)
+#	contents = page.read()
+#	insult = json.loads(contents)
+#	return insult['insult']
 
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
